ff55f905c42c290cd90748ed490d426cc3c8a1da
Martin Karrer <martin@bmalum.com>
2016-08-19 15:19:00 +0200

    Cleanup + Filename Fetching for uploaded.net

--- lib/exHirudo/streamer.ex
+++ lib/exHirudo/streamer.ex
@@ -1,11 +1,9 @@
 defmodule ExHirudo.Streamer do
   use GenServer
   import ExHirudo.Mixfile
-  require(Logger)
 
   def handle_info(%HTTPoison.AsyncStatus{code: _, id: _}, {filename, _}) do
-    file_pid = File.open!(variables[:dl_path]<>filename, [:write, :append, :raw])
-    {:noreply, {filename, file_pid}}
+    {:noreply, {filename, nil}}
   end
 
   def handle_info(%HTTPoison.AsyncChunk{chunk: chunk, id: _}, {filename, file_pid}) do
@@ -16,6 +14,21 @@ defmodule ExHirudo.Streamer do
   def handle_info(%HTTPoison.AsyncEnd{id: _}, {filename, file_pid}) do
     File.close(file_pid)
     {:stop, :normal, :finished, {filename, file_pid}}
+  end
+
+  def handle_info(%HTTPoison.AsyncHeaders{headers: headers}, {filename, file_pid}) do
+    headers_map = Enum.into(headers, %{})
+    condis = Map.get(headers_map, "Content-Disposition")
+    case condis do
+      nil -> filename = "filename.xy"
+      _ ->
+      filename = condis
+      |> String.trim("attachment; filename=\"")
+      |> String.trim_trailing("\"")
+    end
+    IO.inspect filename
+    file_pid = File.open!(variables[:dl_path]<>filename, [:write, :append, :raw])
+    {:noreply, {filename, file_pid}}
   end
 
   def handle_info(_, state) do
