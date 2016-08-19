8afcbca7a4b2a0a3f6e61051a8aeaa690d040eed
Martin Karrer <martin@bmalum.com>
2016-08-19 15:15:42 +0200

    Added wait for Process Shutdown (Pool-Handling) - reindent

--- lib/exHirudo/downloadworker_pool.ex
+++ lib/exHirudo/downloadworker_pool.ex
@@ -5,7 +5,7 @@ defmodule ExHirudo.DownloadWorkerPool do
 
   def init(_) do
     {:ok, pid} = Expool.create_pool(variables[:download_workers], name: :download_worker_pool)
-     Process.register(self(), :download_pool)
+    Process.register(self(), :download_pool)
     {:ok, pid}
   end
 
@@ -16,22 +16,27 @@ defmodule ExHirudo.DownloadWorkerPool do
     GenServer.start_link(__MODULE__, state, opts)
   end
 
-
   def handle_call({:download, url}, _from, pid) do
     Logger.info("Downloading #{url}")
     {:ok, pid} = Expool.submit(:download_worker_pool,
-        fn ->
-          blob_download(url)
-        end)
+    fn ->
+      blob_download(url)
+    end)
     {:reply, pid, []}
   end
 
   # Worker Functions
   def blob_download(url) do
-    Logger.debug(url)
-    Logger.debug(inspect(self()))
-    %HTTPoison.Response{body: body} = HTTPoison.get(url)
-    IO.inspect(body)
+    {:ok, streamer} = GenServer.start(ExHirudo.Streamer, {"test001.zip", []})
+    HTTPoison.get! url, %{}, stream_to: streamer
+    wait_for_shutdown(streamer)
   end
 
+  def wait_for_shutdown(process) do
+    case Process.alive?(process) do
+      true    -> :timer.sleep(1000)
+      wait_for_shutdown(process)
+      _ -> Process.exit(process, :kill)
+    end
+  end
 end
