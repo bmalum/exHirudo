3cc22771c4307b0cae4d9d201413fecd343ead51
Martin Karrer <martin@bmalum.com>
2016-09-01 22:19:14 +0200

    API-Handler with ibrowse and added feature "mark on API" as queued

--- lib/exHirudo/exHirudo_Handler.ex
+++ lib/exHirudo/exHirudo_Handler.ex
@@ -1,17 +1,34 @@
 defmodule ExHirudo.APIHandler do
   import ExHirudo.Mixfile
+  require Logger
 
   def check_for_downloads do
     %HTTPoison.Response{body: body, headers: headers, status_code: status} = HTTPoison.get! variables[:api_endpoint]<>variables[:api_key]
-    url = ExJSON.parse(body, :to_map)
-    |> Dict.get("url")
-    GenServer.call(:download_pool, {:download, url}, 5000)
+    body_enc = ExJSON.parse(body, :to_map)
+    downloading_packages(body_enc)
   end
 
-  def begin_download(download_id) do
+  defp downloading_packages([package | other_packages]) do
+    %{"key" => key, "links" => links, "hash" => hash, "name" => name, "state" => state} = package
+    req_body = ExJSON.generate([data: %{package_name: name, hash: hash, old_state: state, new_state: :queued}])
+    req_url = "http://127.0.0.1:4000/api/v1/"<>variables[:api_key]<>"package/"<>hash<>"/_update_package"
+    req_url = String.to_charlist(req_url)
+    return = :ibrowse.send_req(req_url,[{"Content-Type","application/json"}], :post, req_body)
+    downloading_links(links)
+    downloading_packages(other_packages)
   end
 
-  def finished_download(download_id) do
+  defp downloading_packages([]) do
+    # Terminating the download_packages recursion
+  end
+
+  defp downloading_links([link | other_links]) do
+    GenServer.call(:download_pool, {:download, Dict.get(link, "link")}, 5000)
+    downloading_links(other_links)
+  end
+
+  defp downloading_links([]) do
+    # Terminating the download_links recursion 
   end
 
 end
